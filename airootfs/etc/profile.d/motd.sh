94ae574ee2b2ea57c6dd38dde4dfcae5efa03315
Wojciech Kępka <wojtek.kepka@protonmail.com>
2021-03-18 13:23:46 +0100

    Update motd generator

--- airootfs/etc/profile.d/motd.sh
+++ airootfs/etc/profile.d/motd.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 echo "" > /etc/motd
-echo -e "HOSTNAME:\n$(hotname)" >> /etc/motd
-echo -e "IP ADDRESSES:\n$(ip a | grep inet | cut -d ' ' -f6)" >> /etc/motd
+echo -e "HOSTNAME: $(hostname)" >> /etc/motd
+echo -e "IP ADDRESSES:\n$( ip a | awk -F' ' '/inet / {print "\t" $NF ": " $2}' )" >> /etc/motd
 echo  -e "CPU CORES: $(cat /proc/cpuinfo | grep 'cpu MHz' | wc -l)" >> /etc/motd
-cat /proc/cpuinfo | grep 'cpu MHz' | sort -r | head -1 >> /etc/motd
-cat /proc/meminfo | grep Mem >> /etc/motd
-cat /proc/meminfo | grep 'Swap(T|F)'>> /etc/motd
+cat /proc/cpuinfo | grep 'cpu MHz' | sort -r | awk -F: 'NR==1{print "CPU MHz: " $2}' >> /etc/motd
+cat /proc/meminfo | grep Mem | awk -F: '{gsub(/[ \t]+/, "", $2); print $1 ": " $2}' >> /etc/motd
